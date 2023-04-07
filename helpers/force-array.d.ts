ed2ebf08382d90173156794ed6facff424cc713d
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-04-07 11:17:55 -0300

    Removing `rollup`

--- helpers/force-array.d.ts
+++
@@ -1,2 +0,0 @@
-declare const forceArray: (input: unknown) => any[];
-export default forceArray;
--- helpers/set-backtick.d.ts
+++
@@ -1,2 +0,0 @@
-declare const setBacktick: (tableOrColumn: string) => string;
-export default setBacktick;
